a2c6a2ed4527619a40d756cd6512ec03ebfb2c9f
AniaPiWo <106525407+AniaPiWo@users.noreply.github.com>
2022-11-14 11:14:59 +0100

    comit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,29 +1,39 @@
-const iframe = document.querySelector("iframe");
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
-player.on('play', function() {
-    console.log('played the video!');
-    });
-
-player.getVideoTitle().then(function(title) {
-    console.log('title:', title);
+player.on('play', function () {
+  console.log('played the video!');
 });
 
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
 
+const onPlay = function ({ seconds: currentTime }) {
+  localStorage.setItem(CURRENT, JSON.stringify(currentTime));
+};
 
-/* player.setCurrentTime(localStorage.getItem()).then(function(seconds) {
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player
+  .setCurrentTime(localStorage.getItem())
+  .then(function (seconds) {
     // seconds = the actual time that the player seeked to
-}).catch(function(error) {
+  })
+  .catch(function (error) {
     switch (error.name) {
-        case 'RangeError':
-            // the time was less than 0 or greater than the video’s duration
-            break;
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
 
-        default:
-            // some other error occurred
-            break;
+      default:
+        // some other error occurred
+        break;
     }
-});  */
+  });
 /* 
 1. Zapoznaj się z dokumentacją biblioteki odtwarzacza Vimeo.
 2. Dodaj bibliotekę jako zależność projektu poprzez npm.
